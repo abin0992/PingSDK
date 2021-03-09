053df9717e94dad2f11e8d214870e5e2501e32bb
Abin <abin.baby@riskmethods.net>
2021-03-10 00:25:48 +0100

    Initial Commit

---
+++ PingSDK/PingSDK.h
@@ -0,0 +1,18 @@
+//
+//  PingSDK.h
+//  PingSDK
+//
+//  Created by Abin Baby on 10/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PingSDK.
+FOUNDATION_EXPORT double PingSDKVersionNumber;
+
+//! Project version string for PingSDK.
+FOUNDATION_EXPORT const unsigned char PingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingSDK/PublicHeader.h>
+
+
